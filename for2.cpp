6e8e5cd2acfbb42f70c9bcdeefed83e2d1442802
JacquelineG19 <00036326@uca.edu.sv>
2026-05-18 12:40:29 -0600

    for2 ejercicio10

---
+++ for2.cpp
@@ -0,0 +1,26 @@
+//Serie de Fibonacci
+//Muestra los primeros N términos de la serie de Fibonacci.#include <iostream>
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n,a = 0, b = 1, suma = 0;
+    
+    cout << "Ingrese la cantidad de numero que desea saber: ";
+    cin >> n;
+
+    for ( int i = 1; i <= n; i++){
+
+        cout << a << endl;
+        suma = a + b;
+        a = b;
+        b = suma;
+
+    }
+
+
+
+    return 0;
+}
