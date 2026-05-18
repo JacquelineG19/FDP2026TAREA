a0b5567e90da819aefa1a782011e7e712d58a7ad
JacquelineG19 <00036326@uca.edu.sv>
2026-05-18 12:31:03 -0600

    For 1

---
+++ for1.cpp
@@ -0,0 +1,22 @@
+// Tabla de Multiplicar
+//Pide un número al usuario y muestra su tabla de multiplicar del 1 al 10.
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int n, multiplicacion;
+
+    cout << "Por favor ingrese la tabla de multiplicar que desea saber" << endl;
+    cin >> n;
+
+    for (int i = 1; i <=  10; i++) {
+
+    cout << n << "x" << i << "=" << n * i << endl;
+        
+
+    }
+
+return 0;
+}
